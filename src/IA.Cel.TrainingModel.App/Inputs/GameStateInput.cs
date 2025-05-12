c1dc26826753710641bc6a5046f9025a04d25ef4
Raul <celraul17@gmail.com>
2025-05-12 12:55:32 -0300

    [WIP] IA detection patterns

---
+++ src/IA.Cel.TrainingModel.App/Inputs/GameStateInput.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Data;
+
+namespace IA.Cel.TrainingModel.App.Inputs;
+
+public class GameStateInput
+{
+    [LoadColumn(0)]
+    public string PatternName { get; set; } = string.Empty;
+    
+    [LoadColumn(1)]
+    public string Pattern { get; set; } 
+}
---
+++ src/IA.Cel.TrainingModel.App/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IA.Cel.TrainingModel.App.Inputs;
+using Microsoft.ML;
+
+Console.WriteLine("Hello, World!");
+
+
+var context = new MLContext();
+
+IDataView data = context.Data.LoadFromTextFile<GameStateInput>("data.csv", separatorChar: ',', hasHeader: true);
+
+var pipeline = context.Transforms.Conversion.MapValueToKey("PatternName")
+    .Append(context.Transforms.NormalizeMinMax("Pattern"))
+    .Append(context.MulticlassClassification.Trainers.SdcaMaximumEntropy())
+    .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+
+var model = pipeline.Fit(data);
+var predictions = model.Transform(data);
+var metrics = context.MulticlassClassification.Evaluate(predictions);
+
+Console.WriteLine($"Accuracy: {metrics.MacroAccuracy}");
+
+context.Model.Save(model, data.Schema, "GameOfLifeModel.zip");
+
+Console.WriteLine("end.");
